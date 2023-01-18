5c859727870989081908ed1ce54b6f7d34b6c524
Matias Nahuel Montiel <matiasmontiel213@gmail.com>
2023-01-18 02:50:57 -0300

    primer disenio

--- scripts/script.js
+++ scripts/script.js
@@ -30,32 +30,32 @@ expButton.addEventListener('click', function(){
 
 function show_button(boton){
 	
-	aboutDiv.style.backgroundColor = " #3D1766";
+	aboutDiv.style.backgroundColor = " #976FA7";
 	aboutDiv.style.boxShadow = "none";
 	
-	knowDiv.style.backgroundColor = " #3D1766";
+	knowDiv.style.backgroundColor = " #976FA7";
 	knowDiv.style.boxShadow = "none";
 	
-	expDiv.style.backgroundColor = "#3D1766";
+	expDiv.style.backgroundColor = "#976FA7";
 	expDiv.style.boxShadow = "none";
 	
 	if(boton == 1)
 	{
 		//resaltar about
 		console.log("about!");
-		aboutDiv.style.backgroundColor = "#6F1AB6";
+		aboutDiv.style.backgroundColor = "#613574";
 		aboutDiv.style.boxShadow = "0.2em 0.2em 0.5em rgba(0, 0, 0, 0.5)";
 	}else if(boton == 2)
 	{
 		//resaltar know
 		console.log("know!");
-		knowDiv.style.backgroundColor = "#6F1AB6";
+		knowDiv.style.backgroundColor = "#613574";
 		knowDiv.style.boxShadow = "0.2em 0.2em 0.3em rgba(0, 0, 0, 0.5)";
 	}
 	else if(boton == 3){
 		//resaltar experience
 		console.log("exp!");
-		expDiv.style.backgroundColor = "#6F1AB6";
+		expDiv.style.backgroundColor = "#613574";
 		expDiv.style.boxShadow = "0.2em 0.2em 0.3em rgba(0, 0, 0, 0.5)";
 	}
 }
