0d5ad63bba04226f546ac720b81463d12d27009e
Matias Nahuel Montiel <matiasmontiel213@gmail.com>
2023-01-20 06:22:11 -0300

    disenio final

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ let knowDiv = document.getElementById("know-div");
 
 let expDiv = document.getElementById("exp-div");
 
+//running code? like main
 
 aboutButton.addEventListener('click', function(){
 	show_button(1);
@@ -27,35 +28,40 @@ expButton.addEventListener('click', function(){
 	show_button(3);
 });
 
+//functions
 
 function show_button(boton){
 	
-	aboutDiv.style.backgroundColor = " #976FA7";
-	aboutDiv.style.boxShadow = "none";
+	//hides all divs
+	hide_all();
 	
-	knowDiv.style.backgroundColor = " #976FA7";
-	knowDiv.style.boxShadow = "none";
-	
-	expDiv.style.backgroundColor = "#976FA7";
-	expDiv.style.boxShadow = "none";
-	
+	//but show the one we want, changing style propierites from the DOM
 	if(boton == 1)
-	{
-		//resaltar about
-		console.log("about!");
-		aboutDiv.style.backgroundColor = "#613574";
+	{	
+		aboutDiv.style.backgroundColor = "#8d99ae";
 		aboutDiv.style.boxShadow = "0.2em 0.2em 0.5em rgba(0, 0, 0, 0.5)";
 	}else if(boton == 2)
 	{
-		//resaltar know
-		console.log("know!");
-		knowDiv.style.backgroundColor = "#613574";
+		knowDiv.style.backgroundColor = "#8d99ae";
 		knowDiv.style.boxShadow = "0.2em 0.2em 0.3em rgba(0, 0, 0, 0.5)";
 	}
 	else if(boton == 3){
-		//resaltar experience
-		console.log("exp!");
-		expDiv.style.backgroundColor = "#613574";
+		expDiv.style.display = "block";
+		expDiv.style.backgroundColor = "#8d99ae";
 		expDiv.style.boxShadow = "0.2em 0.2em 0.3em rgba(0, 0, 0, 0.5)";
 	}
+
 }
+
+function hide_all(){
+	aboutDiv.style.backgroundColor = " #2b2d42";
+	aboutDiv.style.boxShadow = "none";
+	
+	knowDiv.style.backgroundColor = " #2b2d42";
+	knowDiv.style.boxShadow = "none";
+	
+	expDiv.style.backgroundColor = "#2b2d42";
+	expDiv.style.boxShadow = "none";
+}
+
+
